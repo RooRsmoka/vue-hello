33e544fa40516ac7b59e8abcfb437eb38c165c67
RooRsmoka <chri.disa96@gmail.com>
2022-01-13 16:27:03 +0100

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// inizializzo Vue JS 
+new Vue({
+    el: "#app", // Aggancio Vue JS nell'elemento html con id 'app'.
+    data: { // salvo nel data le informazioni che verranò richiamate dal html.
+        message: 'Vue js has far more potential than i thought!!!'
+    }
+})
